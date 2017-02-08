1d236942656e572e1a08d18a44f8be80e9d4f6c0
Kapil <ktjbitdoon@gmail.com>
2017-02-08 15:25:06 +0530

    Works with meterial design

--- src/app.component.ts
+++ src/app.component.ts
@@ -4,17 +4,63 @@ import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 
 import 'hammerjs';
-  
-
 
 @Component({
   selector: 'App',
-  templateUrl:'./input-form-example.html',
+  // templateUrl: './input-form-example.html',
   styleUrls: ['./input-form-example.css'],
-  /*template:
-  `<div>
+  template:
+  `
+  <div>
+  <md-toolbar color="primary">
+    <span><md-icon>mood</md-icon></span>
+    <span>Yay, Material in Angular 2!</span>
+    <button md-icon-button [md-menu-trigger-for]="menu">
+      <md-icon>more_vert</md-icon>
+    </button>
+  </md-toolbar>
+  <md-menu x-position="before" #menu="mdMenu">
+    <button md-menu-item>Option 1</button>
+    <button md-menu-item>Option 2</button>
+  </md-menu>
+
+  <md-card>
+    <button md-button>All</button>
+    <button md-raised-button>Of</button>
+    <button md-raised-button color="primary">The</button>
+    <button md-raised-button color="accent">Buttons</button>
+  
+  <h2>Welcome to {{name}} Angular2!</h2>
+  </md-card>
+
+
+  <span class="done">
+    <button md-fab>
+      <md-icon>check circle</md-icon>
+    </button>
+  </span>
+</div>
+  
+  <div>
     <h2>Welcome to {{name}} Angular2!</h2>
-    </div>`*/
+  <button md-icon-button [mdMenuTriggerFor]="menu">
+  <md-icon>more_vert</md-icon>
+</button>
+<md-menu #menu="mdMenu">
+  <button md-menu-item>
+    <md-icon>dialpad</md-icon>
+    <span>Redial</span>
+  </button>
+  <button md-menu-item disabled>
+    <md-icon>voicemail</md-icon>
+    <span>Check voicemail</span>
+  </button>
+  <button md-menu-item>
+    <md-icon>notifications_off</md-icon>
+    <span>Disable alerts</span>
+  </button>
+</md-menu> 
+    </div>`
 })
 export class AppComponent implements OnInit {
   public readonly name = 'electron-forge KT app';
@@ -25,11 +71,9 @@ export class AppComponent implements OnInit {
 }
 
 @NgModule({
-imports: [BrowserModule, MaterialModule.forRoot()],
+  imports: [BrowserModule, MaterialModule.forRoot()],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 
-export class AppModule {
-   
-}
+export class AppModule { }
